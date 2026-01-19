f91d4951a49e3a729969573d2b9d7b80b5d7db0f
Arthur1997Yuki <change9437@gmail.com>
2026-01-19 18:57:50 +0900

    フォルダ構成修正

--- app/Models/Teams/Team.py
+++ app/Models/Teams/Team.py
@@ -3,14 +3,18 @@ import Models.Teams.Abbreviation as Abbreviation
 import Models.HomeTowns.HomeTown as HomeTown
 import ManagementCorporation
 
-class Teams:
+class Team:
     
+    team_id : int
     offical_name : OfficalName
     acronym : Abbreviation
     home_town : HomeTown
     management_corporation : ManagementCorporation
 
-    def __init__(self, offical_name, acronym, home_town, management_corporation):
+    def __init__(self, offical_name, acronym, home_town, management_corporation, team_id=None):
+        if team_id is not None and not isinstance(team_id, int):
+            raise TypeError("チームIDは整数で指定してください。")
+        self.team_id = team_id
         self.offical_name = offical_name
         self.acronym = acronym
         self.home_town = home_town
--- app/db.py
+++ app/db.py
@@ -6,6 +6,8 @@ DATABASE_URL = f'postgresql+psycopg://nwaba:david@127.0.0.1:5432/basketball_db'
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,   # 開発中のスリープ復帰などで壊れた接続を検知
+    pool_size=5,
+    max_overflow=5,
 )
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
